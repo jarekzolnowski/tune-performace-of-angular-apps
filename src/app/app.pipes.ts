1ba3dcb740c6032f0d966b53ceaf8c060e99e670
Jaroslaw Zolnowski <jaroslaw.zolnowski@consultant.volvo.com>
2019-08-16 23:55:00 +0200

    Memoize

--- src/app/app.pipes.ts
+++ src/app/app.pipes.ts
@@ -1,9 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { memoize } from 'lodash-decorators';
 
 @Pipe({
   name: 'isStatusValid'
 })
 export class IsStatusValidPipe implements PipeTransform {
+  @memoize()
   transform(status: number): string {
     console.log('is valid');
     return status === 1 ? 'valid' : 'invalid';
